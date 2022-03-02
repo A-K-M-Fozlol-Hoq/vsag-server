ace30cdcd0e570289836916915dbd9565aea1dbd
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-03-02 23:27:37 +0600

    api workin gnow

--- models/Service.js
+++ models/Service.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const objectId = Schema.ObjectId;
-const serviceSChema = mongoose.Schema(
+const serviceSchema = mongoose.Schema(
   {
     name: {
       type: String,
